c0c891fdb9538dfc9e670e731b140bebbe3b1950
JustynV <justynv@uninorte.edu.co>
2025-03-20 09:07:06 -0500

    main commit

---
+++ project1/src/pages/biling.js
@@ -0,0 +1,7 @@
+import { Navigator } from '../components/navbar.js';
+
+export default function Billing() {
+  return (
+        <Navigator/>
+  );
+}
---
+++ project1/src/pages/features.js
@@ -0,0 +1,7 @@
+import { Navigator } from '../components/navbar.js';
+
+export default function Features() {
+  return (
+        <Navigator/>
+  );
+}
